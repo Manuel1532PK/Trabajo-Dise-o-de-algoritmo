f8b836643279097f12cba411539758089add010c
SebastianCMJ0756 <juansebastiancalderonmartinez@gmail.com>
2025-08-27 18:44:49 -0500

    se agrego comentarios para mejor visualizacion y libreria time para mejor visualizacion en la terminal

--- Trabajo_1/Home.py
+++ Trabajo_1/Home.py
@@ -1,11 +1,23 @@
+# se llama al archivo con todas las funciones
 import Dealer
+import time
 
+# se llama a la funcion para generar el mazo de 52 cartas ordenadas
 Generar_mazo_noOrden= Dealer.generar_mazo()
-print(f"Mazo generado{Generar_mazo_noOrden}")
-print(f"Numero de cartas en el mazo: {len(Generar_mazo_noOrden)}")
+
+print(f"Mazo generado{Generar_mazo_noOrden}") # se muestra maso generado aleatoriamente
+print(f"Numero de cartas en el mazo: {len(Generar_mazo_noOrden)}") # numero total de cartas (52)
+
+# se llama a la funcion que separa las cartas  y las ordena
+# tambien devuelve 4 listas ordenadas
 
 Corazon,Trevol,Diamante,Picas = Dealer.Clasificar_Carta(Generar_mazo_noOrden)
-print(f"\nCorazon: {Corazon}")
-print(f"\nTrevol: {Trevol}")
-print(f"\nDiamante: {Diamante}")
-print(f"\nPica :{Picas}")
+
+# se imprime cada grupo de cartas
+print(f"\nCorazones: {Corazon}")
+time.sleep(2)
+print(f"\nTrevoles: {Trevol}")
+time.sleep(2)
+print(f"\nDiamantes: {Diamante}")
+time.sleep(2)
+print(f"\nPicas: {Picas}")
